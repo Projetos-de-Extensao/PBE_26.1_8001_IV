6cbc5d3308b65a3fb3398ecc8b708143fe443d31
jayp <jpn.athademo@gmail.com>
2026-05-21 22:17:12 -0300

    feat: add documento/relatorio na api

--- app/api.py
+++ app/api.py
@@ -1,12 +1,15 @@
 # app/api.py
 from rest_framework import viewsets
-from app.models import Aluno, Empresa, Instituicao, Orientador, TermoDeCompromisso
+from app.models import Aluno, Empresa, Instituicao, Orientador, TermoDeCompromisso, RelatorioSemestral, Documento
 from app.serializers import (
     AlunoSerializer,
     EmpresaSerializer,
     InstituicaoSerializer,
     OrientadorSerializer,
     TermoDeCompromissoSerializer,
+    DocumentoSerializer,
+    RelatorioSemestralSerializer,
+
 )
 
 class AlunoViewSet(viewsets.ModelViewSet):
@@ -28,3 +31,11 @@ class OrientadorViewSet(viewsets.ModelViewSet):
 class TermoDeCompromissoViewSet(viewsets.ModelViewSet):
     queryset = TermoDeCompromisso.objects.all()
     serializer_class = TermoDeCompromissoSerializer
+
+class DocumentoViewSet(viewsets.ModelViewSet):
+    queryset = Documento.objects.all()
+    serializer_class = DocumentoSerializer
+
+class RelatorioSemestralViewSet(viewsets.ModelViewSet):
+    queryset = RelatorioSemestral.objects.all()
+    serializer_class = RelatorioSemestral
