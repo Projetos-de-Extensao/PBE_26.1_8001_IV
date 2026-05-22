ec4a9d282dcd1326d25cfa998d104f37a4c1a221
jayp <jpn.athademo@gmail.com>
2026-05-21 22:09:05 -0300

    feat: add documento/relatorioSems em admin

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Aluno, Empresa, Instituicao, Orientador, TermoDeCompromisso
+from .models import Aluno, Empresa, Instituicao, Orientador, TermoDeCompromisso, Documento, RelatorioSemestral
 
 admin.site.register(Aluno)
 admin.site.register(Empresa)
 admin.site.register(Instituicao)
 admin.site.register(Orientador)
 admin.site.register(TermoDeCompromisso)
+admin.site.register(Documento)
+admin.site.register(RelatorioSemestral)
